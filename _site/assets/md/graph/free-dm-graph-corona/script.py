df00f8431d639242a0911cd1e1befbf0942a6320
DELPEUCH Sébastien <sedelpeuch@enserib-matmeca.fr>
2020-04-27 11:16:59 +0200

    Mise à jour

--- _site/assets/md/graph/free-dm-graph-corona/script.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-for i in range(1,30):
-    os.system("convert -pointsize 20 -draw 'text 270, 150 \"r = {0} jours\"' tmp_img/courbe_propa_{0}.png {0}".format(i))
