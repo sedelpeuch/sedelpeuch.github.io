876473be231186c398b94e15b3a73b76511635da
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-01-20 20:26:43 +0100

    Update project tags and types, add new projects
    This commit adds tags to identify the ROS, cpp, and python projects. It changes the project type 'Web' to 'Développement', and 'Maker' and 'Autre' types are added. Also, several new projects have been included in the projects list with their respective details provided.

--- data/projects.tsx
+++ data/projects.tsx
@@ -5,7 +5,7 @@ export const projects: Project[] = [
         preview: '/img/project/ronoco.png',
         website: 'https://sedelpeuch.github.io/ronoco/',
         source: 'https://github.com/sedelpeuch/ronoco',
-        tags: ['opensource'],
+        tags: ['opensource', 'ROS'],
         type: 'Robotique',
     },
     {
@@ -14,8 +14,8 @@ export const projects: Project[] = [
         preview: '/img/project/wolf.png',
         website: 'https://github.com/sedelpeuch/wolf',
         source: 'https://github.com/sedelpeuch/wolf',
-        tags: ['opensource'],
-        type: 'Web',
+        tags: ['opensource', 'python'],
+        type: 'Développement',
     },
     {
         title: 'Reachy Mobile',
@@ -23,8 +23,119 @@ export const projects: Project[] = [
         preview: '/img/project/reachy_mobile.png',
         website: 'https://eirlab.github.io/reachy_mobile_reachy/',
         source: 'https://github.com/Eirlab/reachy_mobile_reachy',
-        tags: ['opensource'],
+        tags: ['opensource', 'ROS'],
         type: 'Robotique',
+    },
+    {
+        title: 'EZ Wheel Navigation',
+        description: 'Intégration du Starter Kit EZ Wheel dans ROS',
+        preview: '/img/project/ezwheel.png',
+        website: 'https://eirlab.github.io/reachy_mobile_reachy/ezwheel/ezwheel.html',
+        source: 'https://github.com/Eirlab/reachy_mobile_ezwheel',
+        tags: ['opensource', 'ROS'],
+        type: 'Robotique',
+    },
+    {
+        title: 'Vertical Plotter',
+        description: 'Dessinateur sur tableau blanc',
+        preview: '/img/project/makerplotter.jpg',
+        website: 'https://www.eirlab.net/2021/09/19/vertical-plotter/',
+        source: 'https://github.com/sedelpeuch/MakerPlotter',
+        tags: ['opensource', 'cpp'],
+        type: 'Maker',
+    },
+    {
+        title: 'Template LaTeX',
+        description: "Template LaTeX pour les rapports",
+        preview: '/img/project/latex.png',
+        website: 'https://github.com/sedelpeuch/template_latex',
+        source: 'https://github.com/sedelpeuch/template_latex',
+        tags: ['opensource'],
+        type: 'Autre',
+    },
+    {
+        title: 'Easy Booked @EirLab',
+        description: "Le plugin Easy Booked développé initialement par JoyDevs puis redéveloppé pour les besoins d’inscriptions aux formations d’Eirlab.",
+        preview: '/img/project/easybooked.png',
+        website: 'https://www.eirlab.net/2021/11/03/easy-booked-eirlab/',
+        source: 'https://github.com/Eirlab/easy-booked',
+        tags: ['opensource'],
+        type: 'Développement',
+    },
+    {
+        title: 'Application de suivi - Ultimaker série S',
+        description: "Application de suivi des impressions 3D sur les imprimantes Ultimaker série S",
+        preview: '/img/project/ultimaker.png',
+        website: 'https://www.eirlab.net/2021/11/04/application-de-suivi-ultimaker-serie-s/',
+        source: 'https://github.com/Eirlab/ultimaker-screen',
+        tags: ['opensource'],
+        type: 'Développement',
+    },
+    {
+        title: 'Gérez vos codes sources avec Git',
+        description: "",
+        preview: '/img/project/git.png',
+        website: 'https://www.eirlab.net/2021/11/07/gerez-vos-codes-sources-avec-git/',
+        tags: ['formation'],
+        type: 'Développement',
+    },
+    {
+        title: 'GNU Make',
+        description: "",
+        preview: '/img/project/gnu.png',
+        website: 'https://www.eirlab.net/2021/11/08/gnu-make/',
+        tags: ['formation'],
+        type: 'Développement',
+    },
+    {
+        title: "Recherche de chemin à travers l'algorithme A* en C++",
+        description: "",
+        preview: '/img/project/astar.png',
+        website: 'https://www.eirlab.net/category/cours/astar/',
+        tags: ['formation', 'cpp'],
+        type: 'Développement',
+    },
+    {
+        title: "Traitement d’image pour la détection de tag Aruco avec Opencv en python",
+        description: "",
+        preview: '/img/project/aruco.png',
+        website: 'https://www.eirlab.net/category/cours/aruco/',
+        tags: ['formation', 'python'],
+        type: 'Développement',
+    },
+    {
+        title: "Haricot 🌱 (@apringalle)",
+        description: "Haricot est un prototype d’un boitier basé sur une Raspberry Pi permettant de réaliser automatiquement la vidéo de la pousse d’une plante.",
+        preview: '/img/project/haricot.png',
+        website: 'https://www.eirlab.net/2022/01/09/haricots/',
+        source: 'https://github.com/antoinepringalle/haricot',
+        tags: ['opensource', 'python'],
+        type: 'Développement',
+    },
+    {
+        title: "Luciole ✨ 🐝",
+        description: "Haricot est un prototype d’un boitier basé sur une Raspberry Pi permettant de réaliser automatiquement la vidéo de la pousse d’une plante.",
+        preview: '/img/project/luciole.jpg',
+        website: 'https://www.eirlab.net/2022/07/18/luciole-%e2%9c%a8-%f0%9f%90%9d/',
+        source: 'https://www.eirlab.net/wp-content/uploads/2022/07/Code.zip',
+        tags: ['opensource'],
+        type: 'Maker',
+    },
+    {
+        title: "Imprimantes 3D (SLA)",
+        description: "Eirlab Community met à disposition une imprimante 3D SLA (résine) en libre-service. Pour pouvoir les utiliser en libre-service, il est nécessaire d’avoir suivi une formation dispensée par un FabManager de l’association.",
+        preview: '/img/project/sla.jpg',
+        website: 'https://www.eirlab.net/imprimantes-3d-sla/',
+        tags: ['formation'],
+        type: 'Maker',
+    },
+    {
+        title: "Fraiseuse CNC (Bois)",
+        description: "Ce guide a pour objectif de servir de complément à la formation FAO, il permet de reprendre les différentes étapes nécessaires à la création d’un objet en utilisant une fraiseuse numérique permettant d’usiner du bois ou de l’aluminium. ",
+        preview: '/img/project/fao.jpg',
+        website: 'https://www.eirlab.net/fraiseuse-cnc-bois/',
+        tags: ['formation'],
+        type: 'Maker',
     }
 ]
 
@@ -34,13 +145,15 @@ export type Tag = {
     color: string
 }
 
-export type TagType = 'opensource'
+export type TagType = 'opensource' | 'formation' | 'ROS' | 'cpp' | 'python'
 
-export type ProjectType = 'Robotique' | 'Web'
+export type ProjectType = 'Robotique' | 'Développement' | 'Autre' | 'Maker'
 
 export const projectTypeMap = {
     robotics: 'Robotique',
-    web: 'Web',
+    maker: 'Maker',
+    development: 'Développement',
+    other: 'Autre',
 }
 
 export type Project = {
@@ -58,6 +171,26 @@ export const Tags: Record<TagType, Tag> = {
         label: 'OpenSource',
         description: 'OpenSource project',
         color: '#12affa',
+    },
+    formation: {
+        label: 'Formation',
+        description: 'Formation project',
+        color: '#fa6b12',
+    },
+    ROS: {
+        label: 'ROS',
+        description: 'ROS project',
+        color: '#1225fa',
+    },
+    cpp: {
+        label: 'C++',
+        description: 'C++ project',
+        color: '#243330',
+    },
+    python: {
+        label: 'Python',
+        description: 'Python project',
+        color: '#fab812',
     },
 }
 
