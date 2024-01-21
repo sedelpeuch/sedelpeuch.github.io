0d203f7c2a83ab11aeaf9a491bd06ddcc9a54ff7
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-01-21 13:14:00 +0100

    Implement TechStack component and update about page
    Introduced a new TechStack component to display technology expertise as cards. Updated the about page with the new component and improved person's detail description. Made changes into Jumbotron component configuration for better aesthetics.

---
+++ src/pages/_components/TechStack/index.tsx
@@ -0,0 +1,42 @@
+import {Icon} from '@iconify/react';
+
+// Composant individuel de Card
+function Card({logo, techName}) {
+    return (
+        <div style={{margin: '10px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', textAlign: 'center'}}>
+            <Icon icon={logo} style={{fontSize: '100px'}}/>
+            <div style={{padding: '2px 16px'}}>
+                <h4><b>{techName}</b></h4>
+            </div>
+        </div>
+    );
+}
+
+// Composant de liste de Card
+function TechStack() {
+    const techData = [
+        {name: 'Python', logo: 'skill-icons:python-light'},
+        {name: 'C++', logo: 'logos:c-plusplus'},
+        {name: 'ROS', logo: 'skill-icons:ros-light'},
+        {name: 'ROS2', logo: 'skill-icons:ros-light'},
+        {name: 'Flask', logo: 'skill-icons:flask-light'},
+        {name: 'OpenCV', logo: 'skill-icons:opencv-light'},
+        {name: 'GitHub', logo: 'skill-icons:github-light'},
+        {name: 'Github Actions', logo: 'skill-icons:githubactions-light'},
+        {name: 'JetBrains', logo: 'logos:jetbrains'},
+        {name: 'Oh My Zsh', logo: 'devicon:ohmyzsh'},
+        {name: 'Git', logo: 'skill-icons:git'},
+        {name: 'Docker', logo: 'skill-icons:docker'},
+        {name: 'Linux', logo: 'skill-icons:linux-light'},
+    ];
+
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+            {techData.map((tech, index) =>
+                <Card key={index} logo={tech.logo} techName={tech.name}/>
+            )}
+        </div>
+    );
+}
+
+export default TechStack;
