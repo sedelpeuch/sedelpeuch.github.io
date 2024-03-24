6042f6c74e1a8eb2df7d1f4b9ca40265c224177c
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-03-24 11:28:03 +0100

    Update skill icons and positions

--- data/skills.tsx
+++ data/skills.tsx
@@ -2,41 +2,60 @@ import {IconProps} from '@iconify/react'
 
 const SKILLS: IconProps[] = [
     {
-        icon: 'logos:python',
+        icon: 'skill-icons:python-light',
         style: {left: '15%', top: '15%'},
     },
     {
-        icon: 'devicon:cplusplus',
+        icon: 'skill-icons:cpp',
         style: {left: '24%', top: '20%'},
     },
     {
-        icon: 'skill-icons:github-light',
-        style: {right: '15%', top: '15%'},
+        icon: 'skill-icons:c',
+        style: {left: '21%', top: '5%'},
     },
     {
-        icon: 'logos:jetbrains',
-        style: {right: '20%', top: '20%'},
+        icon: 'skill-icons:github-light',
+        style: {right: '15%', top: '12%'},
     },
-
+    {
+        icon: 'skill-icons:vscode-light',
+        style: {right: '22%', top: '20%'},
+    },
+    {
+        icon: 'skill-icons:gitlab-light',
+        style: {right: '15%', top: '28%'},
+    },
+    {
+        icon: 'skill-icons:git',
+        style: {right: '8%', top: '20%'},
+    },
     {
         icon: 'skill-icons:docker',
-        style: {top: '59%', right: '20%'},
+        style: {top: '58%', right: '20%'},
     },
     {
         icon: 'skill-icons:linux-light',
-        style: {top: '65%', right: '15%'},
+        style: {top: '65%', right: '14%'},
     },
     {
-        icon: 'skill-icons:ros-dark',
-        style: {top: '59%', left: '15%'},
+        icon: 'skill-icons:githubactions-light',
+        style: {top: '65%', right: '26%'},
+    },
+    {
+        icon: 'skill-icons:kubernetes',
+        style: {top: '72%', right: '20%'},
+    },
+    {
+        icon: 'skill-icons:ros-light',
+        style: {top: '65%', left: '26%'},
     },
     {
         icon: 'skill-icons:flask-light',
-        style: {top: '65%', left: '10%'},
+        style: {top: '65%', left: '14%'},
     },
     {
         icon: 'devicon:ohmyzsh',
-        style: {top: '66%', left: '22%'},
+        style: {top: '72%', left: '20%'},
     },
 ]
 
